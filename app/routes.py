424111f8e1aba0a0a87d035d7b8473fc6073415b
Renisha Ferrao <renishaferrao@gmail.com>
2020-10-08 23:52:25 +0530

    Changes in init file

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
-
-
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/home')
@@ -13,9 +12,14 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
-@app.route('/login')
+@app.route('/login',methods=['GET','POST'])
 def log():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/home')
+    return render_template('login.html',form=form)
 @app.route('/register')
 def reg():
     return render_template('reg.html')
